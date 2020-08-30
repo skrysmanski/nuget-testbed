ee519e41843c33d30612221a509d5bc525c5cb98
Sebastian Krysmanski <noreply@manski.net>
2020-08-30 10:46:27 +0200

    Updated base-repo files

--- git-clean.cmd
+++ git-clean.cmd
@@ -1,5 +1,9 @@
 @echo off
 setlocal
+title git clean
+
+powershell -ExecutionPolicy Unrestricted -Command Write-Host -Foreground Cyan 'Removing ignored and untracked files and directories...'
+echo.
 
 :: Delete all ignored files
 git clean -f -X .
@@ -9,3 +13,6 @@ git clean -f -X .
 ::   will effectively delete all empty directories.
 :: NOTE 2: Tracked files with unstaged changes will not be touched.
 git clean -f -d .
+
+echo.
+pause
