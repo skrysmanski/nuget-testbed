39373f8992899eaa928ed07bc86a3d567b4c3e4f
Sebastian Krysmanski <noreply@manski.net>
2020-09-06 06:05:33 +0200

    Fixed git-clean scripts

--- git-clean.cmd
+++ git-clean.cmd
@@ -5,14 +5,11 @@ title git clean
 powershell -ExecutionPolicy Unrestricted -Command Write-Host -Foreground Cyan 'Removing ignored and untracked files and directories...'
 echo.
 
-:: Delete all ignored files
-git clean -f -X .
-
-:: Delete all untracked files and directories.
-:: NOTE: Since directories are never tracked by git, this command
-::   will effectively delete all empty directories.
-:: NOTE 2: Tracked files with unstaged changes will not be touched.
-git clean -f -d .
+:: Delete all untracked and ignored files and directories.
+:: NOTE: We need to use '-x' instead of '-X' here. It's not really clear
+::   what's the difference but when using '-X' some directories don't get
+::   deleted (for example, the "bin/Debug" directories in C# projects).
+git clean -f -d -x "%~dp0\"
 
 echo.
 pause
